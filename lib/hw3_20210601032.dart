bce5a54be93ed3b83748ee7697aa0d312c255235
Havvanur777 <121576054+Havvanur777@users.noreply.github.com>
2026-01-01 19:49:43 +0300

    Task 2

--- lib/hw3_20210601032.dart
+++ lib/hw3_20210601032.dart
@@ -33,6 +33,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   bool isSelected = false;
   bool selectedValue = false;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _textController = TextEditingController();
+  String result = "";
 
   @override
   Widget build(BuildContext context) {
@@ -44,14 +47,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Form(
+        key : _formKey,
         child : Column(
           children: [
             TextFormField(
+              controller : _textController,
               decoration: const InputDecoration(
                 labelText: "Text",
                 contentPadding: EdgeInsets.all(10),
               ),
+              validator: (value){
+                if(value == null || value.isEmpty){
+                  return "You cannot leave this blank";
+                }
+                if(value.length<3){
+                  return "Enter at least 3 letters";
+                }
+                return null;
+              }
             ),
             Checkbox(
               value: isSelected,
@@ -81,13 +95,23 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ElevatedButton(
               onPressed: (){
-
+                if(_formKey.currentState!.validate()){
+                  setState(() {
+                    result = "${_textController.text}\n"
+                             "$isSelected "
+                             "$selectedValue";
+                  });
+                }
               },
               style : ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue,
                 foregroundColor: Colors.white,
               ),
               child: const Text("Send")
+            ),
+            Text(
+              result,
+              textAlign: TextAlign.center,
             ),
           ],
         ),
